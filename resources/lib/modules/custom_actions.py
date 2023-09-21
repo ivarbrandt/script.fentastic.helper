eba11c9fb5c647dd03c7cf51c90e0ef7fffe78d0
Ivar Brandt <ibrandt2@gmail.com>
2023-09-21 17:54:11 -0600

    changed shares to network

--- resources/lib/modules/custom_actions.py
+++ resources/lib/modules/custom_actions.py
@@ -4,7 +4,7 @@ import xbmcgui
 
 def set_image():
     image_file = xbmcgui.Dialog().browse(
-        2, "Choose Custom Background Image", "files", ".jpg|.png|.bmp", False, False
+        2, "Choose Custom Background Image", "network", ".jpg|.png|.bmp", False, False
     )
     if image_file:
         xbmc.executebuiltin("Skin.SetString(CustomBackground,%s)" % image_file)
